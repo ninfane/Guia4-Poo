b087486cf93e836ce3e864e5c9fc6eaf4794a33c
ninfane <marinacrespog@gmail.com>
2021-10-19 17:14:54 -0300

    ex 4 iniciado

---
+++ ex_4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Complejo {
+private:
+	float m_real, m_imag;
+public:
+	Complejo() {}
+	Complejo(float r, float i): m_real(r), m_imag(i) {}
+	
+};
+
+int main() {
+	
+	return 0;
+}
+

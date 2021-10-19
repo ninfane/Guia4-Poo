8adfd621ae11bd5d37f9f47e234c9a09cdb8f36b
ninfane <marinacrespog@gmail.com>
2021-10-19 15:38:11 -0300

    Ex 2 finalizado

--- ex_2.cpp
+++ ex_2.cpp
@@ -14,12 +14,23 @@ bool operator<(Racional a, Racional b){
 	return a.VerNum()*b.VerDen() < a.VerDen()*b.VerNum();
 }
 bool operator>(Racional a, Racional b){
-	return a<b;
+	return b<a;
 }
 bool operator<=(Racional a, Racional b){
+	return not (a>b);
+}
+bool operator>=(Racional a, Racional b){
 	return not (a<b);
 }
+bool operator==(Racional a, Racional b){
+	return a.VerNum()*b.VerDen() == a.VerDen()*b.VerNum();
+}
+bool operator!=(Racional a, Racional b){
+	return not (a==b);
+}
 int main() {
+	//puse los error para chequeos
+	//deberia ir otra cosa
 	
 	Racional a(1,2), b(3,5); //3/5 es mayor que 1/2 (0.6 y 0.5 respec)
 	
@@ -29,8 +40,28 @@ int main() {
 	else cout << "error" << endl;
 	
 	//sobrecarga >
+	//funciona 
 	if(b>a) cout << "b es mayor que a" << endl;
 	else cout << "error" << endl;
+	
+	//sobrecarga <=
+	//funciona 
+	if(a<=b) cout << "a es menor o igual que b" << endl;
+	else cout << "error" << endl;
+	
+	//sobrecarga >=
+	//funciona 
+	if(b>=a) cout << "b es mayor o igual que a" << endl;
+	else cout << "error" << endl;
+	
+	//sobrecarga == 
+	//funciona
+	if(a==b) cout << "a y b son iguales" << endl;
+	else cout << "no son iguales" << endl;
+	
+	//sobrecarga !=
+	if(a!=b) cout << "a y b son distintos" << endl;
+	else cout << "a y b son iguales" << endl;
 	
 	return 0;
 }
